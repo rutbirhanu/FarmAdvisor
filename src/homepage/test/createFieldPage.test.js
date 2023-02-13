4b1c1ff1b000b4530eb49cda927bdada9393f3ab
Kenna-Tefera <annatefera3@gmail.com>
2023-02-13 22:30:16 +0300

    adding test file

---
+++ src/homepage/test/createFieldPage.test.js
@@ -0,0 +1,14 @@
+import '@testing-library/jest-dom';
+import createFieldPage from "../creatFieldPage";
+import {render,screen,cleanup,fireEvent} from'@testing-library/react';
+import {userEvent} from'@testing-library/user-event';
+describe('CreateFieldPage',()=>{ it('should render the same text in to title prop',async() => {
+      render(<CreateFieldPage/>);
+     // const inputElement=screen.getByPlaceholderText(/enter phone Number/i)
+     // const ButtonElemnt=screen.getAllByRole('button',{name:/continue/i});
+     // fireEvent.change(inputElement,{target:{value: '0930961202'}})
+     // fireEvent.click(ButtonElement)
+     // const divElement=screen.getByText(/0930961202/i)
+     // expect(divElement).toBeInTheDocument()
+});
+})
