7b7a49a753faba5568bd1504b38f4d33796d3e4c
rutbirhanu <91417521+rutbirhanu@users.noreply.github.com>
2023-02-13 13:33:29 +0300

    Update selectedfarm.js

--- src/homepage/selectedfarm.js
+++ src/homepage/selectedfarm.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React,  { useState } from 'react'
 import "./homepageStyle.css"
-import { useState } from 'react'
 import CreateFieldModal from './createFieldModal'
 import AppHeader from "../headers/appheader"
 
