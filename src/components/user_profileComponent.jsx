3f59f4b2da15b9d88d63204bb83e6d9c383c33ba
Usmaelabdureman <uabdureman@gmail.com>
2023-02-14 03:01:45 +0300

    user profile component

--- src/components/user_profileComponent.jsx
+++ src/components/user_profileComponent.jsx
@@ -1,29 +1,41 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const UserComponent = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
+ 
   const handleProfileClick = () => {
+    <Link to="/profilePage" ></Link>
+    // navigate('/profile')
+
     // redirect to profile page
   };
 
   const handleHelpClick = () => {
+    <Link to="/helpPage" ></Link>
+    // navigate('/help')
     // redirect to help page
   };
 
   const handleSignOutClick = () => {
     // sign out user and clear any stored data
   };
-
   return (
         
         <div className="menu">
-          <div className='menu-item' onClick={handleProfileClick}>Profile</div>
-          <div className='menu-item' onClick={handleHelpClick}>Help</div>
-          <div className='menu-item' onClick={handleSignOutClick}>Sign Out</div>
+          <div className='menu-item'><Link to="/profilePage" style={styles.linkStyle} >Profile</Link></div>
+          <div className='menu-item text-decoration-none' onClick={handleHelpClick}>Help</div>
+          <div className='menu-item text-decoration-none' onClick={handleSignOutClick}>Sign Out</div>
         
     </div>
   );
 };
 
 export default UserComponent;
+
+const  styles={
+    linkStyle:{
+        textDecoration:'none',
+
+    }
+}
